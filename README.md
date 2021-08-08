818dd51700f7f720905d9156f6f6c6049694bbc9
joehecn <leanbrown@live.cn>
2021-08-09 01:31:42 +0800

    first page

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# 配置密钥
+# lean
+系统第三版记录
+
+## 配置密钥
 Settings -> Secrets -> New repository secret
--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  base: '/lean/'
+  base: '/lean/',
+  lang: 'zh-CN',
+  title: 'lean note v3',
+  description: '系统第三版记录',
+  themeConfig: {
+    repo: 'joehecn/lean',
+    docsDir: 'docs',
+    docsBranch: 'main',
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated'
+  }
 }
